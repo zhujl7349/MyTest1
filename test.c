32f86e7869ea8c72ce35c3191d366481c155cab9
longgegege <734962462@qq.com>
2018-11-13 15:08:21 +0800

    add

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+	printf("hello world...\n");
+	printf("this is test\n");
+	printf("zhujunlong\n");
+	printf("33333\n");
+	printf("currently\n");
+	return 0;
+}
